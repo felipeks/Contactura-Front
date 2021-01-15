bf417c360195576803b36d59a2b142d31cd3218e
felipe Seabra <felipeks25@gmail.com>
2021-01-14 21:48:36 -0300

    feat (Criação): Criação do front contactura

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { CreateEditContactsComponent } from './contacts/create-edit/create-edit.component';
+import { ListContactsComponent } from './contacts/list/list.component';
+import { CreateEditUsersComponent } from './users/create-edit/create-edit.component';
+import { ListUsersComponent } from './users/list/list.component';
+import { Error404Component } from './sharedComponents/error404/error404.component';
+import { NavigationBarComponent } from './sharedComponents/navigation-bar/navigation-bar.component';
+import {MatIconModule} from '@angular/material/icon';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CreateEditContactsComponent,
+    ListContactsComponent,
+    CreateEditUsersComponent,
+    ListUsersComponent,
+    Error404Component,
+    NavigationBarComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
